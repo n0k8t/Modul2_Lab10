2cd6bb1e17f14d99fc20f05e74653bf1851033c5
Timofey Tyrin <timat-ti@mail.ru>
2017-03-23 21:53:12 +0300

    Update TArray.h

--- TArray.h
+++ TArray.h
@@ -14,7 +14,6 @@ template<
 public:
     using value_type = T;
     using size_type	= size_t;
-    using difference_type = std::ptrdiff_t;
     using reference	= value_type&;
     using const_reference = const value_type&;
     using pointer = T*;
